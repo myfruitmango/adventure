582c0db50879c59a8898a1059d52935869648af7
myfruitmango <ctrl.fruitman90@gmail.com>
2023-12-01 14:29:49 +0700

    update routes

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
 import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/home";
+import About from "./pages/about";
+import Page404 from "./pages/404";
 
-function App() {
+const App = () => {
   return (
-    <div>
-      <section>
-        <h1>Hello Welcome To Jungler</h1>
-      </section>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/*" element={<Page404 />} />
+      </Routes>
+    </Router>
   );
-}
+};
 
 export default App;
