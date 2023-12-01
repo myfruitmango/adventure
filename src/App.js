c2ff9f3aa9787c162fe37977c03ccd157cc30fe2
myfruitmango <ctrl.fruitman90@gmail.com>
2023-12-01 10:01:12 +0700

    import components [x] button [x] navbar [x] footer

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
 import "./App.css";
+import Button from "./components/ui/button";
+import Footer from "./components/ui/footer";
+import Navbar from "./components/ui/navbar";
 
-function App() {
+const App = () => {
   return (
     <div>
       <section>
+        <Navbar />
         <h1>Hello Welcome To Jungler</h1>
+        <Button />
+        <Footer />
       </section>
     </div>
   );
-}
+};
 
 export default App;
