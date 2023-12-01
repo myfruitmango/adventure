44aa905bfde47c32a97d5462cb0ed5ffc57ce671
myfruitmango <ctrl.fruitman90@gmail.com>
2023-12-01 10:30:54 +0700

    how to props in react

--- src/App.js
+++ src/App.js
@@ -4,13 +4,32 @@ import Footer from "./components/ui/footer";
 import Navbar from "./components/ui/navbar";
 
 const App = () => {
+  const title = "Mango";
+
+  const clicked = () => {
+    return alert("button dipencet");
+  };
+
+  const Paragraph = () => {
+    return (
+      <div>
+        <i>Mantap Mang</i>
+        {/*
+        ini sebenarnya running tes namun
+        `Do not use <marquee> elements as they can create visual accessibility issues and are deprecated`
+        <marquee>Ini berjalan</marquee>
+        */}
+      </div>
+    );
+  };
+
   return (
     <div>
       <section>
-        <Navbar />
+        <Navbar title={title} />
         <h1>Hello Welcome To Jungler</h1>
-        <Button />
-        <Footer />
+        <Button clicked={clicked} />
+        <Footer paragraf={Paragraph} />
       </section>
     </div>
   );
