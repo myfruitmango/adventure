be5aa504935d6283fee203ff0b0a1167979f9322
myfruitmango <ctrl.fruitman90@gmail.com>
2023-12-01 10:30:24 +0700

    update props in components footer

--- src/components/ui/footer.js
+++ src/components/ui/footer.js
@@ -1,10 +1,19 @@
 import React from "react";
 
 class Footer extends React.Component {
+  /**
+   * !alert
+   * * tidak digunakan lagi saat ini
+   * * jika dihapus tidak masalah karena tidak digunakan
+   */
+  // constructor(props) {
+  //   super(props);
+  // }
   render() {
     return (
       <div>
         <h1>Footer</h1>
+        {this.props.paragraf()}
       </div>
     );
   }
