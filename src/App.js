f5581a299fc161fa226e865207beb5592d8de117
myfruitmango <ctrl.fruitman90@gmail.com>
2023-12-01 14:08:58 +0700

    testing

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
+import React, { useState } from "react";
 import "./App.css";
+import Header from "./components/layouts/header";
 
 function App() {
+  const [getNavbar, setGetNavbar] = useState("");
+
+  const handlerChange = () => {
+    setGetNavbar("My Contact");
+  };
   return (
     <div>
+      <Header value={getNavbar} />
       <section>
         <h1>Hello Welcome To Jungler</h1>
+        <button onClick={() => handlerChange()}>Ubah Navigasi</button>
       </section>
     </div>
   );
